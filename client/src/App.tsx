bf6e0eeac565c1c256e27fdfa6b801d8804cb2c4
RitickLath <ritick943lath@gmail.com>
2025-07-05 15:32:08 +0530

    Pages, Routing init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Problems from "./pages/Problems";
+import ProblemDetail from "./pages/ProblemDetail";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import Submissions from "./pages/Submissions";
+
 const App = () => {
-  return <div className="text-3xl">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/problems" element={<Problems />} />
+        <Route path="/problems/:id" element={<ProblemDetail />} />
+        <Route path="/submissions" element={<Submissions />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/pages/ProblemDetail.tsx
@@ -0,0 +1,5 @@
+const ProblemDetail = () => {
+  return <div>ProblemDetail</div>;
+};
+
+export default ProblemDetail;
